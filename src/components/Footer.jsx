0d90987c33f77da63c3de77e34c92bdd9b384cec
jess <jesswu@gmail.com>
2022-06-28 15:24:37 +0800

    [Added]init pages

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div className='footer'>
+            Jess Wu 2022©
+        </div>
+    );
+};
+
+export default Footer;
---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Search from '../components/Search';
+
+
+const Homepage = () => {
+    const authKey = '563492ad6f91700001000001bc5c0d405e064cf5a42a622ef43072ab';
+  return (
+    <div>Homepage</div>
+  )
+}
+
+export default Homepage
